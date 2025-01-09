58bea69ff7f1502aa5577afa4ba1c5e181b55320
radiippp <riskiadipangestu1@gmail.com>
2025-01-09 22:39:58 +0700

    model user

--- brilink_app/models.py
+++ brilink_app/models.py
@@ -1,3 +1,102 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils import timezone
+from datetime import timedelta
+import uuid
+from django.core.validators import FileExtensionValidator
+from django.core.exceptions import ValidationError
+import random , string
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+ROLE_CHOICES = [
+    ('super_admin', 'Super Admin'),
+    ('admin', 'Admin'),
+    ('teller', 'Teller'),
+    ]
+
+class CreateUpdateTime(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    archive_at = models.DateTimeField(default = None, null=True)
+    deleted_at = models.DateTimeField(default = None, null=True)
+    class Meta:
+        abstract = True
+        
+class Tahun(models.Model):
+    tahun = models.IntegerField(default=2024)
+    class Meta:
+        abstract = True
+
+class Master_UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, username, phone, password, **extra_fields):
+        values = [email, username, phone,]
+        field_value_map = dict(zip(self.model.REQUIRED_FIELDS, values))
+        for field_name, value in field_value_map.items():
+            if not value:
+                raise ValueError('The {} value must be set'.format(field_name))
+
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            username=username,
+            phone=phone,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, username, phone, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_verified', False)
+        return self._create_user(email, username, phone, password, **extra_fields)
+
+    def create_superuser(self, email, username, phone, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_verified', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get('is_verified') is not True:
+            raise ValueError('Superuser must have is_verified=True.')
+
+        return self._create_user(email, username, phone, password, **extra_fields)
+    
+"""TABEL AKUN UNTUK SELAIN BAWAANNYA DJANGO YANG DIPAKAI"""
+class Master_User(AbstractBaseUser, CreateUpdateTime):
+    user_id = models.TextField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    email = models.EmailField(unique=True)
+    username = models.CharField(unique=True, max_length=50)
+    full_name = models.CharField(max_length=50)
+    alamat = models.TextField(blank=True, null=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(null=True)
+    phone = models.CharField(max_length=15)
+    date_of_birth = models.DateField(blank=True, null=True)
+    avatar = models.ImageField(blank=True, null=True, upload_to='images/avatar/', default='images/avatar/default_avatar.png')
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES, default='teller')
+    email_verification_token = models.CharField(max_length=100, default='')
+    
+    objects = Master_UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'phone', 'role', 'is_superuser']
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_short_name(self):
+        return self.first_name
+    
+
---
+++ brilink_app/views/auth.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, HttpResponseBadRequest
+from django.views import View
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.conf import settings
+import shutil,os
+from django.db.models.functions import TruncMonth
+from django.db.models import Sum, Count
+from brilink_app.models import Master_User as m_user, ROLE_CHOICES
+from django.db import transaction
+import re
+
+def check_is_email(email):
+    """
+    Fungsi untuk memeriksa apakah input adalah email atau bukan.
+
+    Args:
+        email (str): String input yang ingin dicek.
+
+    Returns:
+        bool: True jika input adalah email valid, False jika bukan.
+    """
+    email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    return bool(re.match(email_regex, email))
+
+class LoginViews(View):
+    """docstring for LoginViews"""
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return render(request, 'auth/login.html')
+        else:
+            return redirect('app:index_home')
+
+    def post(self, request):
+        if not request.user.is_authenticated:
+            email = request.POST.get('frm_email')
+            pwd = request.POST.get('frm_pwd')
+
+            is_email = check_is_email(email)
+            
+            if is_email:
+                user = authenticate(request, email=email, password=pwd)
+            else:
+                try:
+                    user = m_user.objects.get(username = email, is_active = True)
+                    if not user.check_password(pwd):
+                        user = None
+                except Exception as e:
+                    user = None
+            
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Selamat datang {user.username}")
+                if request.GET.get('next') is not None:
+                    return redirect(request.GET.get('next'))
+                else:
+                    return redirect('app:index_home')
+            else:
+                messages.error(request, "Login gagal, silahkan masukkan data dengan benar")
+                return redirect('app:login')
+        else:
+            return redirect('app:index_home')
+
+@method_decorator(login_required(), name='dispatch')
+class LogoutViews(View):
+    def get(self, request):
+        logout_message = request.GET.get('logout_message', None)
+        if logout_message is not None:
+            messages.info(request, logout_message)
+        
+        logout(request)
+        return redirect(request.META['HTTP_REFERER'])
